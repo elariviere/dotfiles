02fed1a97022f1580554cf22206a05fb756dc990
Eric Lariviere <eric.lariviere@gmail.com>
2019-12-10 13:50:23 -0500

    add ll alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,5 +1,6 @@
 # User specific aliases and functions
 alias ..='cd ..'
+alias ll='ls -l --color=auto'
 alias la='ls -la --color=auto'
 alias ll.='ls -ld .* --color=auto'
 alias cdubi='cd /home/elariviere/workspace/ws_ubiquicast/ubiquicast/'
